0a4970dd12f2cfe1197527fbe223d5b79928802a
Marcus Westbrooks <marcus.westbrooks@gmail.com>
2021-09-11 22:12:57 -0400

    new changes

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ from flask_debugtoolbar import DebugToolbarExtension
 
 from models import connect_db, db, bcrypt, User, FavDrink
 from forms import LoginForm, RegisterForm, DrinkSearch
+import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///drinks"
+uri= os.environ.get('DATABASE_URL', "postgresql:///drinks")
+
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = uri
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = "shhhhh"
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False 
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'hellosecret1')
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 toolbar = DebugToolbarExtension(app)
 
